f1ca421c8d43195c806ba91306085654c78864cf
JavierRDA01 <81706622+JavierRDA01@users.noreply.github.com>
2021-10-19 13:15:53 +0200

    Update Practica_Tenis.cpp

--- Practica_Tenis.cpp
+++ Practica_Tenis.cpp
@@ -13,42 +13,57 @@ const int ANCHO_PISTA = 7;
 int posicion_bola = 4;
 int posicion_tenista = 4;
 
-void introducirDato(string jugador1, string jugador2, int velocidad, int habilidad) {
-	cout << "Introduce el nombre del jugador 1: ";
-	cin >> jugador1;
-	cout << "Introduce el nombre del jugador 2: ";
-	cin >> jugador2;
-	cout << "Introduce la velocidad del jugador (Intervalo 1-3): ";
-	cin >> velocidad;
-	cout << "Introduce la habilidad del jugador (Intervalo 1-3): ";
-	cin >> habilidad;
-	if (habilidad >= LIM_INF_HAB && habilidad <= LIM_SUP_HAB) {
-		cout << "Cargando...";
-	}
-	else if (velocidad <= LIM_INF_VEL && velocidad <= LIM_SUP_VEL) {
-		cout << "Cargando...";
-	}
-	else {
-		cout << "Valores introducidos incorrectos, pruebe otra vez" << endl;
-	}
 
-}
-
-string marcador(int puntuacion) {
-	if (puntuacion == 0) {
+string marcador(int puntuacion_jug_1, int puntuacion_jug_2) {
+	if (puntuacion_jug_1 == 0) {
 		cout << "00";
 	}
-	else if (puntuacion == 1) {
+	else if (puntuacion_jug_1 == 1) {
 		cout << "15";
 	}
-	else if (puntuacion == 2) {
+	else if (puntuacion_jug_1 == 2) {
 		cout << "30";
 	}
-	else if (puntuacion == 3) {
+	else if (puntuacion_jug_1 == 3) {
 		cout << "40";
 	}
-	else if (puntuacion == 4) {
+	else if (puntuacion_jug_1 == 4) {
 		cout << "[Ventaja]";
+	}
+	if (puntuacion_jug_2 == 0) {
+		cout << "00";
+	}
+	else if (puntuacion_jug_2 == 1) {
+		cout << "15";
+	}
+	else if (puntuacion_jug_2 == 2) {
+		cout << "30";
+	}
+	else if (puntuacion_jug_2 == 3) {
+		cout << "40";
+	}
+	else if (puntuacion_jug_2 == 4) {
+		cout << "[Ventaja]";
+	}
+}
+void elegirGanador(string jugador1, string jugador2, int puntuacion_jug_1, int puntuacion_jug_2) {
+	int eleccion;
+	eleccion = rand() % 3;
+	while (eleccion != 0) {
+		if (eleccion == 1) {
+			cout << "Gana " << jugador1 << endl;
+			puntuacion_jug_1++;
+			cout << puntuacion_jug_1 << "||" << puntuacion_jug_2 << endl;
+		}
+		else if (eleccion == 2) {
+			cout << "Gana " << jugador2 << endl;
+			puntuacion_jug_2++;
+			cout << puntuacion_jug_1 << "||" << puntuacion_jug_2 << endl;
+		}
+		else if (eleccion == 0) {
+			cout << "No gana nadie" << endl;
+			cout << puntuacion_jug_1 << "||" << puntuacion_jug_2 << endl;
+		}
 	}
 }
 int golpeoBola(int posicion_tenista, int habilidad) {
@@ -61,10 +76,15 @@ int golpeoBola(int posicion_tenista, int habilidad) {
 		probabilidad_exito = (100 - ((posicion_randm - habilidad) / ((ANCHO_PISTA - 1) - LIM_INF_HAB) * 100));
 		if (acierto < probabilidad_exito) {
 			cout << "Acierto";
+			return 0;
 		}
 		else {
 			cout << "Error";
+			return 0;
 		}
+	}
+	else {
+		return 0;
 	}
 }
 void elegirSaque(string jugador1, string jugador2) {
@@ -97,15 +117,35 @@ int correTenista(int posicion_tenista, int velocidad, int posicion_bola) {
 	}
 }
 int main() {
-	int habilidad, velocidad, puntuacion, posicion_tenista, posicion_bola;
+	int habilidad, velocidad, puntuacion_jug_1, puntuacion_jug_2, posicion_tenista, posicion_bola;
 	string jugador1, jugador2;
+	puntuacion_jug_1 = 0;
+	puntuacion_jug_2 = 0;
+	posicion_tenista = 4;
+	posicion_bola = 4;
 	srand(time(NULL));
 	system("chcp 1252");
 	system("cls");
-	introducirDato(jugador1, jugador2, habilidad, velocidad);
+	cout << "Introduce el nombre del jugador 1: ";
+	cin >> jugador1;
+	cout << "Introduce el nombre del jugador 2: ";
+	cin >> jugador2;
+	cout << "Introduce la velocidad del jugador (Intervalo 1-3): ";
+	cin >> velocidad;
+	cout << "Introduce la habilidad del jugador (Intervalo 1-3): ";
+	cin >> habilidad;
+	if (habilidad >= LIM_INF_HAB && habilidad <= LIM_SUP_HAB) {
+		cout << "Cargando...";
+	}
+	else if (velocidad <= LIM_INF_VEL && velocidad <= LIM_SUP_VEL) {
+		cout << "Cargando...";
+	}
+	else {
+		cout << "Valores introducidos incorrectos, pruebe otra vez" << endl;
+	}
 	elegirSaque(jugador1, jugador2);
 	golpeoBola(habilidad, posicion_tenista);
 	correTenista(posicion_tenista, posicion_bola, velocidad);
-	marcador(puntuacion);
+	marcador(puntuacion_jug_1, puntuacion_jug_2);
 	return 0;
 }
